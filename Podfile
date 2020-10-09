be2fd783cbf4b65a2847165855daba704a7d1fcf
Toon Wong <toonwwk@hotmail.com>
2020-10-09 22:20:00 +0700

    add template ui

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WongnaiIntern' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WongnaiIntern
+
+end
---
+++ WongnaiIntern/DummyTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  DummyTableViewCell.swift
+//  WongnaiIntern
+//
+//  Created by Kanokporn Wongwaitayakul on 9/10/2563 BE.
+//  Copyright © 2563 Kanokporn Wongwaitayakul. All rights reserved.
+//
+
+import UIKit
+
+class DummyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
